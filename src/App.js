8c4df3c93ad78e9d6e41ddc2384e4c5d9566cce6
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2017-12-01 14:10:26 +0900

    add filter function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import MyForm from './MyForm';
+import MyForm from './components/MyForm';
+import ToggleButton from './components/ToggleButton';
 import v4 from 'uuid/v4'
 
 class App extends Component {
@@ -9,21 +10,22 @@ class App extends Component {
     this.state = {
       tasks: [
         {
-          id: "1",
+          id: v4(),
           completed: false,
           description: "My todo 1"
         },
         {
-          id: "2",
+          id: v4(),
           completed: false,
           description: "My todo 2"
         },
         {
-          id: "3",
+          id: v4(),
           completed: false,
           description: "My todo 3"
         }
-      ]
+      ],
+      current: 'all'
     }
   }
 
@@ -44,12 +46,16 @@ class App extends Component {
   }
 
   render() {
-    const { tasks } = this.state
+    const { tasks, current } = this.state
     const that = this
     return (
       <div>
         <MyForm myEvent={description => that.addTask(description)} />
-        <ul> {tasks.map(({ id, completed, description }) => (
+        <ul> {tasks.filter(({ completed }) => {
+          if (current === 'done') return completed
+          else if (current === 'not yet') return !completed
+          else if (current === 'all') return true
+        }).map(({ id, completed, description }) => (
           <li key={id}
             style={{
               textDecoration: completed ? 'line-through' : 'none'
@@ -59,9 +65,13 @@ class App extends Component {
             {description}
           </li>
         )
-        )
+          )
         }
         </ul>
+        <p>{this.state.current}</p>
+        <ToggleButton onClick={prev => this.setState({ ...prev, current: "done" })}>done</ToggleButton>
+        <ToggleButton onClick={prev => this.setState({ ...prev, current: "not yet" })}>not yet</ToggleButton>
+        <ToggleButton onClick={prev => this.setState({ ...prev, current: "all" })}>all</ToggleButton>
       </div>
     );
   }
