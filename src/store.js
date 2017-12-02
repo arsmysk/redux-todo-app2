aac861e96a7c43f6a3f0112a95f71e7e888e05ac
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2017-12-02 22:41:46 +0900

    Add thunk middleware from scratch

--- src/store.js
+++ src/store.js
@@ -1,29 +1,24 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 
-const addLoggingToDispatch = (store) => (rawDispatch) => {
-  return (action) => {
-    console.group(action.type)
-    console.log('%c prev state', 'color: gray', store.getState())
-    console.log('%c action', 'color: blue', action)
-    const returnValue = rawDispatch(action)
-    console.log('%c next state', 'color: green', store.getState())
-    console.groupEnd(action.type)
-    return returnValue
-  }
+const addLoggingToDispatch = (store) => (rawDispatch) => (action) => {
+  console.group(action.type)
+  console.log('%c prev state', 'color: gray', store.getState())
+  console.log('%c action', 'color: blue', action)
+  const returnValue = rawDispatch(action)
+  console.log('%c next state', 'color: green', store.getState())
+  console.groupEnd(action.type)
+  return returnValue
 }
 
-const addPromiseToDispatch = (store) => (rawDispatch) => {
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch)
-    } else {
-      return rawDispatch(action)
-    }
-  }
-}
+const addPromiseToDispatch = (store) => (rawDispatch) => (action) =>
+  typeof action.then === 'function' ? action.then(rawDispatch) : rawDispatch(action)
 
-const middlewares = [addPromiseToDispatch, addLoggingToDispatch]
+const thunk = (store) => (rawDispatch) => (action) =>
+  typeof action === 'function' ? action(rawDispatch) : rawDispatch(action)
+
+
+const middlewares = [thunk, addPromiseToDispatch, addLoggingToDispatch]
 let store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 export default store
