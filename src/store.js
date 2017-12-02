27d77670d1bc152bf7e4a21c650dd3ee75fb4939
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2017-12-02 20:36:40 +0900

    Add store logging system

--- src/store.js
+++ src/store.js
@@ -1,4 +1,19 @@
 import { createStore } from 'redux'
 import rootReducer from './reducers'
 
-export default createStore(rootReducer)
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch
+  return (action) => {
+    console.group(action.type)
+    console.log('%c prev state', 'color: gray', store.getState())
+    console.log('%c action', 'color: blue', action)
+    const returnValue = rawDispatch(action)
+    console.log('%c next state', 'color: green', store.getState())
+    console.groupEnd(action.type)
+    return returnValue
+  }
+}
+
+let store = createStore(rootReducer)
+store.dispatch = addLoggingToDispatch(store)
+export default store
