ecd8b70df42db276554d2a2f2c4e987bacf983c0
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2017-12-02 23:03:53 +0900

    remove promise  middleware from action creater

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,7 @@
 import TYPE from './_actionTypes'
 
 export const addTodoAction = description => ({ type: TYPE.ADD_TODO, payload: { description } })
-export const toggleTodoAction = id => new Promise((resolve) => {
-  setTimeout(() => {
-    resolve({ type: TYPE.TOGGLE_TODO, payload: { id } })
-  }, 500)
-})
+export const toggleTodoAction = id => dispatch => setTimeout(() => {
+  dispatch({ type: TYPE.TOGGLE_TODO, payload: { id } })
+}, 500)
 export const currentAction = current => ({ type: TYPE.CURRENT, payload: { current } })
--- src/store.js
+++ src/store.js
@@ -4,13 +4,10 @@ import rootReducer from './reducers'
 /* Logging middleware */
 import addLoggingToDispatch from 'redux-logger'
 
-/* recognizing Promise object middleware */
-import addPromiseToDispatch from 'redux-promise'
-
 /* thunk middleware */
 import thunk from 'redux-thunk'
 
-const middlewares = [thunk, addPromiseToDispatch, addLoggingToDispatch]
+const middlewares = [thunk, addLoggingToDispatch]
 let store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 export default store
