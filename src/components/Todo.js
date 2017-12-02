718ab4a6fe3101ed84c086bff0939df699bc3614
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2017-12-02 20:40:47 +0900

    Allow to async action

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,12 +2,23 @@ import React from 'react'
 import { toggleTodoAction } from '../actions'
 import store from '../store'
 
+const asyncProc = (id) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(id)
+    }, 500)
+  })
+}
+
+
 export default ({ attr: { id, completed, description } }) => {
   return (
     <li style={{
       textDecoration: completed ? 'line-through' : 'none'
     }}
-      onClick={() => store.dispatch(toggleTodoAction(id))}
+      onClick={() =>
+        asyncProc(id).then((asyncId) => store.dispatch(toggleTodoAction(asyncId))
+        )}
     >
       {description}
     </li>
