71a94a30a6cb0f4aa633cda1b549a1db8f7e2965
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2017-12-09 19:12:22 +0900

    install react-router

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store'
 import { Provider } from 'react-redux'
+import Home from './components/Home'
+import Sub from './components/Sub'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 ReactDOM.render((
-  <Provider store={store} >
-    <App />
-  </Provider>), document.getElementById('root'));
-
+  <div>
+    <Provider store={store} >
+      <App />
+    </Provider>
+    <Router>
+      <div>
+        <Route exact path='/' component={Home} />
+        <Route path='/sub' component={Sub} />
+        <Link to={''}>Home</Link>
+        <Link to={'sub'}>Sub</Link>
+      </div>
+    </Router>
+  </div>), document.getElementById('root'));
 registerServiceWorker();
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 /* Logging middleware */
 import addLoggingToDispatch from 'redux-logger'
@@ -7,7 +8,16 @@ import addLoggingToDispatch from 'redux-logger'
 /* thunk middleware */
 import thunk from 'redux-thunk'
 
-const middlewares = [thunk, addLoggingToDispatch]
-let store = createStore(rootReducer, applyMiddleware(...middlewares))
+let middlewares = [thunk]
+
+let composeMiddlewares
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(addLoggingToDispatch)
+  composeMiddlewares = composeWithDevTools(applyMiddleware(...middlewares))
+} else {
+  composeMiddlewares = applyMiddleware(...middlewares)
+}
+
+let store = createStore(rootReducer, composeMiddlewares)
 
 export default store
